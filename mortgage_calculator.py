93339652c8fd7cee217dcab1cb4b225bf2a7dfe8
Sonali Agnihotri <sonaliagnihotri2416@gmail.com>
2026-07-17 22:34:45 +0530

    Calculation Part

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -23,7 +23,7 @@ interest_rate = st.number_input(
     value=8.5,
     step=0.1
 )
-
+#calculation part
 loan_years = st.number_input(
     "Loan Duration (Years)",
     min_value=1,
@@ -31,4 +31,14 @@ loan_years = st.number_input(
     value=20,
     step=1
 )
+monthly_rate = interest_rate / 100 / 12
 
+months=loan_years*12
+#EMI formula
+factor = (1 + monthly_rate) ** months
+numerator = loan_amount * monthly_rate * factor
+denominator = factor - 1
+monthly_payment = numerator / denominator
+
+st.subheader("Monthly Mortgage Payment")
+st.metric("EMI", f"₹{monthly_payment:,.2f}")
